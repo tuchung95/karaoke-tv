217ce5465c0316c1852184c98c7a8bb111e4bb08
tuchung95 <chungtrantuantu@gmail.com>
2026-08-29 16:43:36 +0700

    Phiên bản 1.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.athr.karaoketv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.15"
+        versionCode = 17
+        versionName = "1.16"
     }
 
     signingConfigs {
